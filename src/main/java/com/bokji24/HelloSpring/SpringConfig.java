5a99c648fab7278c2df4a7a16e977c1bc99e8b51
Mineru98 <rmstjr1030@naver.com>
2021-12-06 17:14:00 +0900

    Apply JDBC Template

--- src/main/java/com/bokji24/HelloSpring/SpringConfig.java
+++ src/main/java/com/bokji24/HelloSpring/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.bokji24.HelloSpring;
 
 import com.bokji24.HelloSpring.repository.JdbcMemberRepository;
+import com.bokji24.HelloSpring.repository.JdbcTemplateMemberRepository;
 import com.bokji24.HelloSpring.repository.MemberRepository;
 import com.bokji24.HelloSpring.repository.MemoryMemberRepository;
 import com.bokji24.HelloSpring.service.MemberService;
@@ -28,6 +29,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
